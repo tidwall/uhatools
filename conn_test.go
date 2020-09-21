96f207c63962a2e6a33d787496f190a4d5cbb47c
tidwall <joshbaker77@gmail.com>
2020-09-21 16:13:02 -0700

    Added go.mod file

--- conn_test.go
+++ conn_test.go
@@ -1,9 +1,7 @@
 package uhatools
 
 import (
-	"fmt"
 	"testing"
-	"time"
 )
 
 func TestClient(t *testing.T) {
@@ -12,23 +10,6 @@ func TestClient(t *testing.T) {
 		Auth:      "hello",
 	})
 	defer cl.Release()
-
 	c := cl.Get()
 	defer c.Release()
-
-	// c, err := Dial(":11001", "hello", nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// defer c.Close()
-	for {
-		v, err := c.Do("select count(*) from user")
-		if err != nil {
-			println(err.Error())
-		} else {
-			fmt.Printf("%s\n", v)
-		}
-		time.Sleep(time.Second / 2)
-	}
-
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tidwall/uhatools
+
+go 1.15
+
+require (
+	github.com/gomodule/redigo v1.8.2
+	github.com/google/btree v1.0.0
+)
